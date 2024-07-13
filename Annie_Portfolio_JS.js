a5ae136df795da93a4cbc64d7f45a360d775565f
Annie Prasanna <anniemanoharan44@gmail.com>
2024-07-14 01:03:15 +0530

    changed file names

---
+++ Annie_Portfolio_JS.js
@@ -0,0 +1,42 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navBar');
+
+menuIcon.onclick = () => {
+   menuIcon.classList.toggle('bx-x');
+   navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+   sections.forEach(sec =>{
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+
+      if(top >= offset && top <offset + height){
+         navLinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+         });
+      };
+   });
+};
+
+let darkModeIcon = document.querySelector('#darkMode-icon');
+darkModeIcon.onclick = () => {
+   darkModeIcon.classList.toggle('bx-sun');
+   document.body.classList.toggle('dark-mode');
+};
+
+ScrollReveal({
+   reset : true,
+   distance : '80px',
+   duration : 1000,
+   delay : 100
+});
+
+ScrollReveal().reveal('.home-content .heading', {origin: 'top'});
+// ScrollReveal().reveal('.home-img img, .project-list', '.form', {origin: 'bottom'});
